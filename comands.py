8d0882a61a75256378cb8cb8de9184fc89302754
Elizaveta <77387348+lisonka12@users.noreply.github.com>
2021-10-04 10:00:26 +0300

    Added commands

---
+++ comands.py
@@ -0,0 +1,4 @@
+curl -i https://api.github.com/users/octocat/repos > myrepos.json
+
+curl -i -H "Authorization: token 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" \
+    https://api.github.com/user/repos > repos_token.json
